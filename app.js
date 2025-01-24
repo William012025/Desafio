29ddd0ee030a2c93665c602e5d4429b670217082
William012025 <wabbr@yahoo.com.br>
2025-01-24 19:15:38 -0300

    dia 2 - função mostrar nomes no html

--- app.js
+++ app.js
@@ -1,37 +1,67 @@
 let amigos = [];
 
 
-amigos = ['joao', 'maria'];
-console.log(amigos);
+amigos = ['mae', 'maria'];
+//console.log(amigos);
 
 
 function adicionarAmigo() {
-    
-   
+
+
     let incluirAmigo = document.querySelector('input').value;
-    if (incluirAmigo.trim() === '' ){
+       if (incluirAmigo.trim() === '' ){
         alert('Por favor, insira um nome.');
         return;
     }        
     
     amigos.push(incluirAmigo);
+    listaComHTML();
+    
     console.log(amigos);
-limparNome ();
+    limparNome ();
 
 }
-
-
 
 console.log(amigos);
 
 
-function limparNome() {
-    incluirAmigo = document.querySelector('input');
-    incluirAmigo.value = '';
+function listaComHTML() {
+
+    let lista = document.querySelector("#listaAmigos");  // ser for por id colocar #listaAmigos
+    lista.innerHTML = '';
+    for (i = 0; i < amigos.length; i++) {
     
+    let novoNaLista = document.createElement('li');
+        novoNaLista.textContent = amigos[i];
+        lista.appendChild(novoNaLista);
+    }
+    
+    
+
+    
+/*
+        for (let i = 0; i < amigos.length; i++) {
+            let li = document.createElement('li');
+            li.textContent = amigos[i];
+            lista.appendChild(li);
+    }
+    amigos.
 }
 
 
+/*function verificaListaAmigos() {
+    let amigosNaLista = amigos.document.querySelector('input').value
+    amigosNaLista.includes();
+    return verificaListaAmigos();
+    */
+}
+
+
+
+
+function limparNome() {
+    document.querySelector('input').value = '';
+}
 
 
 function sortearAmigo() {
